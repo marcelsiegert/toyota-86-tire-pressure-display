ed541b5fb2e7b8bad521ab47cea5d0319dacc7a4
Marcel Siegert <mdotsiegert@gmail.com>
2024-10-02 23:30:50 +0200

    Initialize display

--- toyota-86-tire-pressure-display.ino
+++ toyota-86-tire-pressure-display.ino
@@ -1,5 +1,17 @@
+#include <Adafruit_ST7789.h>
+
+static int8_t constexpr PIN_LCD_CS = 10;
+static int8_t constexpr PIN_LCD_DC = 7;
+static int8_t constexpr PIN_LCD_RST = 8;
+
+static Adafruit_ST7789 LCD(PIN_LCD_CS, PIN_LCD_DC, PIN_LCD_RST);
+
 void setup()
 {
+  LCD.init(172, 320);
+  LCD.enableDisplay(false);
+  LCD.fillScreen(ST77XX_BLACK);
+  LCD.enableDisplay(true);
 }
 
 void loop()
