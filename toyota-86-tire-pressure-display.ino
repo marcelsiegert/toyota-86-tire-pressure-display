6c0b5988433670d0984d5ab88e8e253d345a5b76
Marcel Siegert <mdotsiegert@gmail.com>
2026-04-16 00:41:09 +0200

    Use ClangFormat and the WebKit style

--- toyota-86-tire-pressure-display.ino
+++ toyota-86-tire-pressure-display.ino
@@ -9,22 +9,20 @@ static Adafruit_ST7789 LCD(PIN_LCD_CS, PIN_LCD_DC, PIN_LCD_RST);
 
 void setup()
 {
-  LCD.init(172, 320);
-  LCD.enableDisplay(false);
-  LCD.fillScreen(ST77XX_BLACK);
-  LCD.enableDisplay(true);
+    LCD.init(172, 320);
+    LCD.enableDisplay(false);
+    LCD.fillScreen(ST77XX_BLACK);
+    LCD.enableDisplay(true);
 
-  CAN.setFilterMask_Standard(0x1FFC0000);
-  for (size_t mailbox = 0; mailbox < R7FA4M1_CAN::CAN_MAX_NO_STANDARD_MAILBOXES; mailbox++)
-  {
-    CAN.setFilterId_Standard(mailbox, 0x758);
-  }
-  if (!CAN.begin(CanBitRate::BR_500k))
-  {
-    LCD.print("Error initializing CAN");
-    for (;;)
-      ;
-  }
+    CAN.setFilterMask_Standard(0x1FFC0000);
+    for (size_t mailbox = 0; mailbox < R7FA4M1_CAN::CAN_MAX_NO_STANDARD_MAILBOXES; mailbox++) {
+        CAN.setFilterId_Standard(mailbox, 0x758);
+    }
+    if (!CAN.begin(CanBitRate::BR_500k)) {
+        LCD.print("Error initializing CAN");
+        for (;;)
+            ;
+    }
 }
 
 void loop()
